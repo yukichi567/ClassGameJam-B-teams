6a70345bc0ee0bcd3cea2dd7266e664a3fe53f82
ShiratoriYuu <shiratoriyu0124@gmail.com>
2022-12-16 16:21:00 +0900

    test

---
+++ Assets/Shiratori/enmiy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enmiy : MonoBehaviour
+{
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+    }
+}
