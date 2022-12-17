48f36a49ea1d760ca86cacc8ff54369c7668470d
ShiratoriYuu <shiratoriyu0124@gmail.com>
2022-12-17 17:37:21 +0900

    敵のSC

--- Assets/Shiratori/enmiy.cs
+++ Assets/Shiratori/enmiy.cs
@@ -2,20 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enmiy : MonoBehaviour
+public class enmiy: MonoBehaviour
 {
-    public float speed = 10.0f;
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 pos;
+    public int muki = 1; //方向
+    Vector3 syoki;
+
+    private void Start()
     {
-        
+        syoki = transform.position; //初期座標
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float translation = Input.GetAxis("Horizontal") * speed;
-        translation *= Time.deltaTime;
-        transform.Translate(translation, 0, 0);
+        pos = transform.position;
+        
+        transform.Translate(transform.right * Time.deltaTime * 5 * muki); //速度と方向を決める　
+
+        if (pos.x > syoki.x + 4)//一番右に行った時の座標
+        {
+            muki = -1;
+        }
+        if (pos.x < syoki.x - 4) //一番左に行った時の座標
+        {
+            muki = 1;
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void End()
+    {
+       // gameObject.SetActive
+    }
+
 }
